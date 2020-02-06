27dafe3197e5c5de300432cec26d72765cf1e5ae
isa <isaemilia.h@gmail.com>
2020-02-06 14:51:09 +0200

    Fail .

--- script.js
+++ script.js
@@ -30,25 +30,44 @@ console.log('hello')
 
 // Gallery
 
-let modal = document.getElementById('myModal');
-let img = document.getElementById('myImg');
-let modalImg = document.getElementById('img01');
-let captionTxt = document.getElementById('caption');
-let span = document.getElementsByClassName('close')[0];
-
-img.onclick = function() {
-    modal.style.display = 'block';
-    modalImg.src = this.alt;
-    captionTxt.innerHTML = this.alt;
-
-}
-
-span.onclick = function(){
-    modal.style.display = 'none'
-}
-
-
-function openImg() {
-
-}
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 
