5106fdf96f2fa27f1f19b2bdf734a5422f138480
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2020-02-07 13:52:37 +0200

    Update script.
    Brought it up to date

--- script.js
+++ script.js
@@ -54,10 +54,10 @@ function openModal() {
   }
   
   function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
+   let i;
+   let slides = document.getElementsByClassName("mySlides");
+   let dots = document.getElementsByClassName("demo");
+   let captionText = document.getElementById("caption");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
