d7cb18ba3f314b913f9ea52f0439101458413769
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2020-02-26 14:04:50 +0200

    Add a lot of changes and a galleryStyles.css
    Trying to figure out functions and organizing some stuff

--- script.js
+++ script.js
@@ -4,70 +4,64 @@ const nav = document.querySelector('#nav');
 let topOfNav = nav.offsetTop;
 let header = document.getElementById('header')
 
-function fixNav() {
+function fixNav(callMe) {
     if (window.scrollY >= topOfNav) {
         document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.body.classList.add('fixed-nav');
         header.classList.add('hidden');
     } 
+  }
+
+
+function betterFixNav() {
+
+
 }
 
+window.addEventListener ('', betterFixNav)
 window.addEventListener('scroll', fixNav);
 
 
-// Code for opening tabs from nav bar
+// // Gallery
 
-const tab = document.getElementsByClassName('tab-content')
-const tabLink = document.getElementsByClassName('tablink')
-const defaultOpen = document.getElementById('defaultOpen')
-
-
-function openTab() {
-
-console.log('hello') 
-
-}
-
-// Gallery
-
-// Open the Modal
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
+// // Open the Modal
+// function openModal() {
+//     document.getElementById("myModal").style.display = "block";
+//   }
   
-  // Close the Modal
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
+//   // Close the Modal
+//   function closeModal() {
+//     document.getElementById("myModal").style.display = "none";
+//   }
   
-  var slideIndex = 1;
-  showSlides(slideIndex);
+//   var slideIndex = 1;
+//   showSlides(slideIndex);
   
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+//   // Next/previous controls
+//   function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
   
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//   // Thumbnail image controls
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
   
-  function showSlides(n) {
-   let i;
-   let slides = document.getElementsByClassName("mySlides");
-   let dots = document.getElementsByClassName("demo");
-   let captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+//   function showSlides(n) {
+//    let i;
+//    let slides = document.getElementsByClassName("mySlides");
+//    let dots = document.getElementsByClassName("demo");
+//    let captionText = document.getElementById("caption");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+//     captionText.innerHTML = dots[slideIndex-1].alt;
+//   }
 
