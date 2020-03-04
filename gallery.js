f838d4ca78c1b2c54ad14ea789ddaef5c4f77277
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2020-03-04 12:04:14 +0200

    Add internal screming.
    Help meh Thomas pls

--- gallery.js
+++ gallery.js
@@ -6,15 +6,15 @@ function fixNav(callMe) {
     if (window.scrollY >= topOfNav) {
         document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.body.classList.add('fixed-nav');
-        
+
     } else {
-      document.body.classList.remove('fixed-nav')
-      document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav')
+        document.body.style.paddingTop = 0;
     }
-  } 
+}
 
 
-window.addEventListener ('load', fixNav)
+window.addEventListener('load', fixNav)
 window.addEventListener('scroll', fixNav);
 
 
@@ -24,21 +24,38 @@ const elements = document.getElementsByClassName('column');
 
 
 function one() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "100%";
-  } console.log('i work');
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "100%";
+    }
+    console.log('i work');
 }
 
 // Two images side by side
 function two() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "50%";
-  }console.log('i work too');
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "50%";
+    }
+    console.log('i work too');
 }
 
 // Four images side by side
 function four() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "25%";
-  }
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
 }
+
+
+// Modal styling
+
+const modal = document.getElementById('myModal');
+//const image = document.querySelectorAll('open-modal');
+const captionText = document.getElementById('caption');
+const span = document.getElementsByClassName('close')[0];
+
+const image = document.querySelectorAll('.open-modal').addEventListener('click', function () {
+        console.log('works');
+    });
+
+// !image.style.display === "none" ? image.style.display = "block" : 
+// image.style.display = "none"
